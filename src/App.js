86183acb7c5f261802aa016a1b5a98b571edeae8
Austin-Weng <73495049+Austin-Weng@users.noreply.github.com>
2024-04-29 20:38:32 -0700

    Got it to finally work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="headerContainer">
+        <h1 className="header">Acropolis Robotics Center</h1>
+      </div>
+      <Router>
+        <nav style={{ backgroundColor: '#598cde', color: 'white' }}>
+          <ul style={{ listStyleType: 'none', display: 'flex' }}>
+            <li><Link to="/" style={{ color: 'white', padding: 10 }}>Home</Link></li>
+            <li><Link to="/guides" style={{ color: 'white', padding: 10 }}>Guides</Link></li>
+            <li><Link to="/trello" style={{ color: 'white', padding: 10 }}>Trello</Link></li>
+            <li><Link to="/timeline" style={{ color: 'white', padding: 10 }}>Timeline</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/guides" component={Guides} />
+          <Route path="/trello" component={Trello} />
+          <Route path="/timeline" component={Timeline} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div className="screen">
+      <p>Home Screen</p>
+    </div>
+  );
+}
+
+function Guides() {
+  return (
+    <div className="screen">
+      <p>Guides Screen</p>
+    </div>
+  );
+}
+
+function Trello() {
+  return (
+    <div className="screen">
+      <iframe title="Trello" style={{ width: '100%', height: '100%' }} src="https://trello.com" />
+    </div>
+  );
+}
+
+function Timeline() {
+  return (
+    <div className="screen">
+      <p>Timeline</p>
     </div>
   );
 }
