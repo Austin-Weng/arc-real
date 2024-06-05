5b6a4ca0bd34156262f45697fae59dca076f4d03
Austin-Weng <73495049+Austin-Weng@users.noreply.github.com>
2024-06-04 18:26:13 -0700

    Changed README

--- src/Timeline.js
+++ src/Timeline.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const Timeline = () => {
   return (
-    <div>
+    <div className='container'>
       <h1 id='timelineHeader'>Timeline</h1>
       <iframe title="timeline page" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSFM4rifl6p3A-UFKfFVG1-jrSMndxI4Mb6SKBx0Dmq6sGqJqhyvGmAOw1q3CmpXmQD9oXaULUc1xAy/pubhtml?gid=1316494279&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
     </div>
